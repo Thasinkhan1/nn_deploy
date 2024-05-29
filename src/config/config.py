94dca04243bfe6f2ebff13916f2d9aedc50c1738
THASINKHAN1 <thasinkhan249@gmail.com>
2024-05-29 12:29:25 +0530

    added config package along with its module and created preprocessing pakage

---
+++ src/config/config.py
@@ -0,0 +1,23 @@
+NUM_INPUTS = 2
+NUM_LAYERS = 3
+P = [NUM_INPUTS,2,1] # NO OF PERCEPTRON
+
+
+
+f = [None,"LINEAR","SIGMOID","RELU","LEAKY RELU"] # list of activation
+
+LOSS_FUNCTION = ["MEAN SQUARED ERROR"]
+MINI_BATCH_SIZE = 1
+# theta0 = [None]
+# theta = [None]
+
+# z = [None]*NUM_LAYERS #aoutomatically have three time of None
+# h = [None]*NUM_LAYERS
+
+
+# del_fl_by_del_z = [None]*NUM_LAYERS
+# del_hl_by_del_theta0 = [None]*NUM_LAYERS
+# del_hl_by_del_theta = [None]*NUM_LAYERS
+# del_L_by_del_h = [None]*NUM_LAYERS
+# del_L_by_del_theta = [None] *NUM_LAYERS
+# del_L_by_del_theta0 =  [None] *NUM_LAYERS
---
+++ src/preprocessing/data_management.py
@@ -0,0 +1,4 @@
+from config import config
+# so its create a codebase
+# git commit -m "added config package along with its module and created preprocessing pakage"
+config.f
